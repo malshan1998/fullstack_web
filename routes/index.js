6e0914744b0595c2858a34872f945a9ab021afc4
malshan1998 <malshanranawella@gmail.com>
2023-06-12 15:22:07 +0530

    Integrating routes with views

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router() // Get the Router variable from express.
 
 router.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('index')
 })
 
 module.exports = router     // To export the router variable to be used in the server.js file.
